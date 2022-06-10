a7b4024f7c7658bb6d01e05096a38d0db6e7e7ae
Mike Doyle <mikedoyle007@gmail.com>
2022-06-09 21:43:30 -0700

    Add neovim config files

---
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -0,0 +1,113 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Move text up and down
+keymap("n", "<ALT-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("n", "<ALT-k>", "<Esc>:m .-2<CR>==gi", opts)
+
+-- Insert --
+-- Press kj or jk fast to enter
+keymap("i", "jk", "<ESC>", opts)
+keymap("i", "kj", "<ESC>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+
+-- Maintains yanked register when pasting over another word
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Terminal --
+-- Better terminal navigation
+-- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+-- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+-- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+-- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- Telescope --
+-- Find file name
+keymap("n", "<C-p>", "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>", opts)
+-- Grep word in files with live_grep
+-- keymap("n", "<C-f>", "<cmd>lua require('telescope.builtin').live_grep(require('telescope.themes').get_dropdown{previewer = false})<cr>", opts)
+keymap("n", "<C-f>", "<cmd>lua require('telescope.builtin').live_grep(require('telescope.themes').get_ivy())<cr>", opts)
+-- Custom for config files
+keymap("n", "<leader>sd", "<cmd>lua require('user.customTelescope').edit_neovim()<cr>", opts)
+-- Fuzzy Find - Current Buffer ( <C-/>)  On M1 Macs this is actually <C-/>, but on Intel Mac it is <C-_>
+-- keymap("n", "<C-_>", "<cmd>lua require('user.customTelescope').curr_buff()<cr>", opts)
+keymap("n", "<C-/>", "<cmd>lua require('user.customTelescope').curr_buff()<cr>", opts)
+
+-- Vim Fugitive --
+-- keymap("n", "<leader>gs", ":G<CR>", opts)
+
+-- Neogit --
+keymap("n", "<leader>gs", ":Neogit<CR>", opts)
+
+-- Diffview --
+-- keymap("n", "<C-S-G>", ":DiffviewOpen<CR>", opts)
+-- I have this mapped in whichkey as well, keeping this as a backup
+keymap("n", "<C-g>", ":DiffviewClose<CR>", opts)
+
+-- Quickly save and execute (source) a file
+keymap("n", "<leader>x", ":wa<CR>:so %<CR>", opts)
+
+-- Bufferline Close a buffer
+keymap("n", "<leader>w", ":bd<CR>", opts)
+
+-- Goto previous file
+-- TODO: preserve position
+keymap("n", "<leader>p", "<C-^><CR>", opts)
+
+-- Reload init.lua
+-- TODO: This is not reloading currently because package.loaded.customTelescope is already nil
+keymap("n", "<F4>", ":lua package.loaded.customTelescope = nil <cr>:source ~/.config/nvim/init.lua<cr>", opts)
+
+-- Splits --
+-- Vertical Split
+keymap("n", "<leader>\\", ":vsp<CR>", opts)
+
---
+++ nvim/.config/nvim/lua/user/options.lua
@@ -0,0 +1,61 @@
+local options = {
+  hidden = true,                           -- required to keep multiple buffers open
+  backup = false,                          -- creates a backup file
+  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
+  cmdheight = 1,                           -- more space in the neovim command line for displaying messages (used to be set to 2)
+  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+  conceallevel = 0,                        -- so that `` is visible in markdown files
+  fileencoding = "utf-8",                  -- the encoding written to a file
+  hlsearch = false,                        -- highlight all matches on previous search pattern
+  incsearch = true,                        -- show search results with each keystroke
+  ignorecase = true,                       -- ignore case in search patterns
+  mouse = "a",                             -- allow the mouse to be used in neovim
+  pumheight = 10,                          -- pop up menu height
+  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
+  smartcase = true,                        -- smart case
+  splitbelow = true,                       -- force all horizontal splits to go below current window
+  splitright = true,                       -- force all vertical splits to go to the right of current window
+  swapfile = false,                        -- creates a swapfile
+  termguicolors = true,                    -- set term gui colors (most terminals support this)
+  timeoutlen = 500,                        -- time to wait for a mapped sequence to complete (in milliseconds)
+  undofile = true,                         -- enable persistent undo
+  updatetime = 300,                        -- faster completion (4000ms default)
+  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+  smarttab = true,                         -- makes tabbing smarter, will realize you have 2 vs 4
+  expandtab = true,                        -- convert tabs to spaces
+  showtabline = 2,                         -- always show tabs
+  tabstop = 2,                             -- insert 2 spaces for a tab
+  smartindent = true,                      -- make indenting smarter again
+  autoindent = true,                       -- good auto indent
+  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
+  cursorline = true,                       -- highlight the current line
+  number = true,                           -- set numbered lines
+  relativenumber = false,                  -- set relative numbered lines
+  numberwidth = 4,                         -- set number column width to 2 {default 4}
+  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
+  wrap = false,                            -- display lines as one long line
+  scrolloff = 8,                           -- is one of my fav
+  sidescrolloff = 8,
+  guifont = "monospace:h17",               -- the font used in graphical neovim applications
+  -- I added these...
+  inccommand = "split",                       -- shows partial off-screen results in a preview window
+  -- guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20",
+  guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20",
+  winbar = "%=%m %f",                      -- Shows file path in top right corner with modified flag [+]
+}
+
+vim.opt.shortmess:append "c"
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
+vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set iskeyword+=-]]
+vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+
+-- Cool highlighting courtesy of @clason
+vim.cmd [[augroup LuaHighlight]]
+vim.cmd [[  au!]]
+vim.cmd [[  au TextYankPost * silent! lua return (not vim.v.event.visual) and require'vim.highlight'.on_yank()]]
+vim.cmd [[augroup END]]
